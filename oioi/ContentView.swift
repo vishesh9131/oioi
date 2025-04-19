b491099aed7bc88b442f1b46e40f4064f1c51bb6
Vishesh <87526302+vishesh9131@users.noreply.github.com>
2025-04-19 13:25:49 +0530

    Initial Commit

---
+++ oioi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  oioi
+//
+//  Created by Vishesh Yadav on 19/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ oioiTests/oioiTests.swift
@@ -0,0 +1,17 @@
+//
+//  oioiTests.swift
+//  oioiTests
+//
+//  Created by Vishesh Yadav on 19/04/25.
+//
+
+import Testing
+@testable import oioi
+
+struct oioiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
